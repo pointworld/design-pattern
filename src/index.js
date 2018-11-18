c88084ed5d16080f5de3b75cbc9c768635c201be
point <onerf@sina.com>
2018-11-18 09:36:35 +0800

    playground: state pattern

--- src/index.js
+++ src/index.js
@@ -16,4 +16,5 @@
 // import './designPattern/structural/proxyPattern/demo/es6Proxy'
 // import './designPattern/behavioral/observerPattern'
 // import './designPattern/behavioral/iteratorPattern'
-import './designPattern/behavioral/iteratorPattern/demo/es6-iterator'
+// import './designPattern/behavioral/iteratorPattern/demo/es6-iterator'
+import './designPattern/behavioral/statePattern'
